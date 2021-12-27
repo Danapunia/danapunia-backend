23bd9d0670e7ccadaf2f95ea41e966f074f27975
tutdeputraw <anoboy.code@gmail.com>
2021-12-27 16:43:59 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const db = require('./src/models/_index');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+require('./src/routes/routes')(app);
+
+db.sequelize.sync().then(result => {
+  app.listen(process.env.SERVER_PORT || 3000);
+  console.log('server is running on port ' + process.env.SERVER_PORT);
+}).catch(err => {
+  console.log(err);
+});
